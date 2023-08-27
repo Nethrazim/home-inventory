c2a9619f9393b8e540e20ef593fd7ef00519d47a
Nehtrazim <vlad.branzei@gmail.com>
2023-08-27 17:08:43 +0300

    + produces response type

--- HomeInsideOut/HomeInsideOut.Common.Api/Controllers/BaseController.cs
+++ HomeInsideOut/HomeInsideOut.Common.Api/Controllers/BaseController.cs
@@ -1,7 +1,10 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using HomeInsideOut.Common.Api.Responses;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,7 +12,10 @@ namespace HomeInsideOut.Common.Api.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
+    [ProducesResponseType(typeof(BaseResponse), StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(typeof(BaseResponse), StatusCodes.Status404NotFound)]
+    [ProducesResponseType(typeof(BaseResponse), StatusCodes.Status500InternalServerError)]
     public class BaseController : ControllerBase
-    {
+    { 
     }
 }
